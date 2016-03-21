cae6feb279b348b8d4b76a27f09166ac9048a712
ViperX12 <giop1r8@yahoo.com>
2016-03-20 22:19:18 -0400

    Initial Commit

---
+++ Terraria-crafting-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Terraria-crafting-app
+//
+//  Created by Giovanni on 2016-03-20.
+//  Copyright © 2016 Giovanni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
